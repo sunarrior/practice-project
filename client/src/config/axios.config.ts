ecaa5d7fb05daff7eec0ad614f94c35340bea2bf
nhat <nhatthuynhh621@gmail.com>
2023-03-19 14:13:43 +0700

    complete user profile function

--- client/src/config/axios.config.ts
+++ client/src/config/axios.config.ts
@@ -1,10 +1,9 @@
 /* eslint-disable @typescript-eslint/dot-notation */
 import axios, { AxiosInstance } from "axios";
 
-// const token: string | null = localStorage.getItem("token");
-// const config = {
-//   headers: { Authorization: `Bearer ${token}` },
-// };
+// const token: string | null =
+//   typeof window !== undefined ? window.localStorage.getItem("token") : null;
+// const config = token ? { headers: { Authorization: `Bearer ${token}` } } : null;
 
 const API: AxiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
